e789c23c168d6f372ffb2be5e26e1978302af7bb
Maximenavellier <maximenavellier12@gmail.com>
2025-10-21 09:24:01 +0200

    Add files via upload

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+
+const users: { name: string; email: string }[] = []; // Stockage en mémoire
+
+export const getUsers = (req: Request, res: Response) => {
+ res.json({ users });
+};
+
+export const addUser = (req: Request, res: Response) => {
+ const { name, email } = req.body;
+
+ if (!name || !email) {
+   return res.status(400).json({ message: "Nom et email requis" });
+ }
+ users.push({ name, email }); // Ajout en mémoire
+ console.log("→ Utilisateur ajouté :", { name, email });
+ res.json({ message: `Utilisateur ${name} ajouté avec succès !`, email });
+};
